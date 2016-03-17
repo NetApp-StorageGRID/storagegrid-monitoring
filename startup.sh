475ebb99f839d08d2fc8eaaa865eae63f263092a
Clemens Siebler <clemens.siebler@gmail.com>
2016-03-17 10:44:02 +0100

    Updated dashboards

--- startup.sh
+++ startup.sh
@@ -4,14 +4,23 @@ docker-compose up &
 
 sleep 10
 
-# Add Elasticsearch as datasource to Grafana
+# Add Elasticsearch datasources to Grafana
 curl -X POST 'http://admin:admin@localhost:3000/api/datasources' \
      -H 'Content-Type: application/json;charset=UTF-8' \
-     --data-binary '{"name":"elasticsearch","type":"elasticsearch","access":"proxy", "url":"http://elasticsearch:9200","password":"","user":"", "database":"logstash-*","basicAuth":false,"basicAuthUser":"","basicAuthPassword":"", "withCredentials":false,"isDefault":false,"jsonData":null}}'
+     --data-binary '{"name":"es-sgaudit","type":"elasticsearch","access":"proxy", "url":"http://elasticsearch:9200","password":"","user":"", "database":"logstash-*","basicAuth":false,"basicAuthUser":"","basicAuthPassword":"", "withCredentials":false,"isDefault":false,"jsonData":null}}'
+
+curl -X POST 'http://admin:admin@localhost:3000/api/datasources' \
+     -H 'Content-Type: application/json;charset=UTF-8' \
+     --data-binary '{"name":"es-sgapi","type":"elasticsearch","access":"proxy", "url":"http://elasticsearch:9200","password":"","user":"", "database":"counters-*","basicAuth":false,"basicAuthUser":"","basicAuthPassword":"", "withCredentials":false,"isDefault":false,"jsonData":null}}'
 
 # Make sure datasource is added and initialized
 sleep 2
 
-# Import Dashboard
+# Import Main Dashboard
 curl -X POST 'http://admin:admin@localhost:3000/api/dashboards/db' -H 'Content-Type: application/json;charset=UTF-8' -d @grafana/dashboards/init.json
 curl -X POST 'http://admin:admin@localhost:3000/api/dashboards/db' -H 'Content-Type: application/json;charset=UTF-8' -d @grafana/dashboards/storagegrid-webscale.json
+
+# Import Next-gen Dashboard
+
+curl -X POST 'http://admin:admin@localhost:3000/api/dashboards/db' -H 'Content-Type: application/json;charset=UTF-8' -d @grafana/dashboards/init-monitoring.json
+curl -X POST 'http://admin:admin@localhost:3000/api/dashboards/db' -H 'Content-Type: application/json;charset=UTF-8' -d @grafana/dashboards/storagegrid-webscale-monitoring.json
--- update-dashboard.sh
+++ update-dashboard.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 curl -X GET 'http://admin:admin@localhost:3000/api/dashboards/db/storagegrid-webscale' -H 'Content-Type: application/json;charset=UTF-8' > grafana/dashboards/storagegrid-webscale.json
+curl -X GET 'http://admin:admin@localhost:3000/api/dashboards/db/storagegrid-webscale-monitoring' -H 'Content-Type: application/json;charset=UTF-8' > grafana/dashboards/storagegrid-webscale-monitoring.json
